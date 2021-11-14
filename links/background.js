26fa9bdf8859ddbad278aee3998f64179db77c50
Spencer Churchill <spencer.l.churchill@gmail.com>
2021-11-14 14:11:22 -0800

    migrate to manifest v3

--- links/background.js
+++ links/background.js
@@ -1,22 +1,21 @@
-chrome.browserAction.onClicked.addListener(function(activeTab) {
+chrome.action.onClicked.addListener((tab) => {
   chrome.tabs.create({
-    url: 'index.html'
+    url: "index.html",
   });
 });
 
-chrome.runtime.onInstalled.addListener(function(details){
-  if(details.reason == 'install'){
-    localStorage.setItem('grad1', '#0066cc');
-    localStorage.setItem('grad2', '#cccccc');
-    localStorage.setItem('deg', '135deg');
-    localStorage.setItem('text', '#ffffff');
-    localStorage.setItem('logourl', 'images/logos/logo.svg');
-    localStorage.setItem('flagurl', 'images/flags/united%20nations.svg');
-    localStorage.setItem('countval', 'Model United Nations');
-    localStorage.setItem('gavel','x');
-    localStorage.setItem('flagchange','x');
-  }
-  else{
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason == "install") {
+    localStorage.setItem("grad1", "#0066cc");
+    localStorage.setItem("grad2", "#cccccc");
+    localStorage.setItem("deg", "135deg");
+    localStorage.setItem("text", "#ffffff");
+    localStorage.setItem("logourl", "images/logos/logo.svg");
+    localStorage.setItem("flagurl", "images/flags/united%20nations.svg");
+    localStorage.setItem("countval", "Model United Nations");
+    localStorage.setItem("gavel", "x");
+    localStorage.setItem("flagchange", "x");
+  } else {
     return;
   }
 });
